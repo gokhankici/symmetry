78fd5f359b75d02b6a48635a30bff79db9b5812c
Rami Gökhan Kıcı <gokhankici@gmail.com>
2016-09-22 15:14:00 -0700

    using lift @ Ping00

--- checker/tests/pos/Ping00.hs
+++ checker/tests/pos/Ping00.hs
@@ -1,28 +1,29 @@
 {-# Language RebindableSyntax #-}
 {-# Language ScopedTypeVariables #-}
 {-# Language FlexibleContexts #-}
+{-# Language DataKinds #-}
 module Main where
 
 import Prelude hiding ((>>=), (>>), fail, return)
 import Symmetry.Language
 import Symmetry.Verify
 
-pingServer :: (DSL repr) => repr (Process repr ())
-pingServer = do myPid <- self
+pongServer :: (DSL repr) => repr (Process repr ())
+pongServer = do myPid <- self
+                (p :: repr (T "Ping" (Pid RSing))) <- recv
                 -- yield G: PtrR[p] = 0
                 --       R: PtrW[p] > 0
-                p     <- recv
-                send p myPid
+                send (forget p) (lift (TyName :: TyName "Pong") myPid)
                 -- exit G: PtrW[p] > 0
 
 master :: (DSL repr) => repr
           (RSing -> Process repr ())
-master = lam $ \r -> do p     <- spawn r pingServer
+master = lam $ \r -> do p     <- spawn r pongServer
                         myPid <- self
-                        _     <- send p myPid
+                        send p (lift (TyName :: TyName "Ping") myPid)
                         -- yield G: PtrW[p] > 0, PtrR[master] = 0
                         --       R: PtrW[master] > 0
-                        _ :: repr (Pid RSing) <- recv
+                        (_ :: repr (T "Pong" (Pid RSing))) <- recv
                         return tt
                         -- exit G: PtrR[master] = 1...
 
